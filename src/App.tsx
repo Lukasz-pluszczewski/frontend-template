0875601fbd425f76041bbbdd5c549ab61fd1ddac
lukasz <lukasz.pluszczewski@gmail.com>
2023-12-22 12:42:51 +0100

    Updated packages, added old colors to theme

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,23 @@ import '@mantine/core/styles.css';
 
 const queryClient = new QueryClient();
 
-const theme = createTheme({});
+// Set colors from before https://github.com/mantinedev/mantine/releases/tag/7.3.0
+const theme = createTheme({
+  colors: {
+    dark: [
+      "#C1C2C5",
+      "#A6A7AB",
+      "#909296",
+      "#5c5f66",
+      "#373A40",
+      "#2C2E33",
+      "#25262b",
+      "#1A1B1E",
+      "#141517",
+      "#101113"
+    ],
+  }
+});
 const colorSchemeManager = localStorageColorSchemeManager({ key: 'mantine-color-scheme' });
 
 const ColorSchemeControls = ({ children }: { children: ReactNode }) => {
